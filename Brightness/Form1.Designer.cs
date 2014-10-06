b121e4c7407603f3f92a0f6d35067dcf31016282
alonso <alonso@wavegis.com.tw>
2014-10-06 15:49:03 +0800

    initial

---
+++ Brightness/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Brightness
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brightLabel = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.brightValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // brightLabel
+            // 
+            this.brightLabel.AutoSize = true;
+            this.brightLabel.Location = new System.Drawing.Point(13, 13);
+            this.brightLabel.Name = "brightLabel";
+            this.brightLabel.Size = new System.Drawing.Size(56, 13);
+            this.brightLabel.TabIndex = 0;
+            this.brightLabel.Text = "Brightness";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(76, 13);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 100;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "value:";
+            // 
+            // brightValue
+            // 
+            this.brightValue.AutoSize = true;
+            this.brightValue.Location = new System.Drawing.Point(230, 13);
+            this.brightValue.Name = "brightValue";
+            this.brightValue.Size = new System.Drawing.Size(25, 13);
+            this.brightValue.TabIndex = 3;
+            this.brightValue.Text = "100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 50);
+            this.Controls.Add(this.brightValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.brightLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label brightLabel;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label brightValue;
+    }
+}
+
---
+++ Brightness/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+namespace Brightness
+{
+    public partial class Form1 : Form
+    {
+        private Monitor _currentMonitor;
+        private readonly MonitorCollection _monitorCollection = new MonitorCollection();
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.ValueChanged += trackBar1_ValueChanged;
+            var @delegate = new NativeMethods.MonitorEnumDelegate(MonitorEnum);
+            NativeMethods.EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, @delegate, IntPtr.Zero);
+        }
+
+        void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            brightValue.Text = trackBar1.Value.ToString();
+            UpdateScreenWithBarValue((uint) trackBar1.Value);
+        }
+
+        private void UpdateScreenWithBarValue(uint value)
+        {
+            _currentMonitor = _monitorCollection[0];
+            NativeMethods.SetMonitorBrightness(_currentMonitor.HPhysicalMonitor, value);
+        }
+        // To be called by a delegate
+        private bool MonitorEnum(IntPtr hMonitor, IntPtr hdcMonitor, ref Rectangle lprcMonitor, IntPtr dwData)
+        {
+            _monitorCollection.Add(hMonitor);
+            return true;
+        }
+    }
+}

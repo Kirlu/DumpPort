a2a31c3b61e926ed96e7b21908adcdbaceddc8e7
alonso <alonso@wavegis.com.tw>
2014-10-07 10:59:35 +0800

    add audioVolumnChange project

---
+++ AudioVolumnChange/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AudioVolumnChange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.trackBar1.Location = new System.Drawing.Point(0, 0);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(295, 45);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(13, 50);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonUp.TabIndex = 1;
+            this.buttonUp.Text = "up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(208, 50);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 23);
+            this.buttonDown.TabIndex = 2;
+            this.buttonDown.Text = "down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 85);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+    }
+}
+
---
+++ AudioVolumnChange/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+//using CoreAudioApi;
+using NAudio;
+
+namespace AudioVolumnChange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.ValueChanged += trackBar1_ValueChanged;
+            try
+            {
+                //Instantiate an Enumerator to find audio devices
+                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                //Get all the devices, no matter what condition or status
+                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                //Loop through all devices
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        dev.AudioEndpointVolume.MasterVolumeLevel = 0;
+                        Trace.WriteLine("MasterVolumeLevel:"+dev.AudioEndpointVolume.StepInformation.StepCount);
+                        trackBar1.Minimum = (int) dev.AudioEndpointVolume.VolumeRange.MinDecibels;
+                        trackBar1.Maximum = (int) dev.AudioEndpointVolume.VolumeRange.MaxDecibels;
+                        trackBar1.Scale(dev.AudioEndpointVolume.MasterVolumeLevelScalar);
+                        trackBar1.Value = (int) dev.AudioEndpointVolume.MasterVolumeLevel;
+                        //Set at maximum volume
+                        //dev.AudioEndpointVolume.MasterVolumeLevel = 0;
+
+                        //Get its audio volume
+                        System.Diagnostics.Debug.Print("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevel.ToString());
+
+                        //Mute it
+                        //dev.AudioEndpointVolume.Mute = true;
+                        System.Diagnostics.Debug.Print(dev.FriendlyName + " is muted");
+                    }
+                    catch (Exception ex)
+                    {
+                        //Do something with exception when an audio endpoint could not be muted
+                        System.Diagnostics.Debug.Print(dev.FriendlyName + " could not be muted");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //When something happend that prevent us to iterate through the devices
+                System.Diagnostics.Debug.Print("Could not enumerate devices due to an excepion: " + ex.Message);
+            }
+        }
+
+        void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            /*
+            try
+            {
+                //Instantiate an Enumerator to find audio devices
+                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                //Get all the devices, no matter what condition or status
+                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                //Loop through all devices
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        dev.AudioEndpointVolume.MasterVolumeLevel=trackBar1.Value;
+                        Trace.WriteLine(trackBar1.Value);
+                        //Set at maximum volume
+                        //dev.AudioEndpointVolume.MasterVolumeLevel = 0;
+
+                        //Get its audio volume
+                        System.Diagnostics.Debug.Print("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevel.ToString());
+
+                        //Mute it
+                        //dev.AudioEndpointVolume.Mute = true;
+                        System.Diagnostics.Debug.Print(dev.FriendlyName + " is muted");
+                    }
+                    catch (Exception ex)
+                    {
+                        //Do something with exception when an audio endpoint could not be muted
+                        System.Diagnostics.Debug.Print(dev.FriendlyName + " could not be muted");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //When something happend that prevent us to iterate through the devices
+                System.Diagnostics.Debug.Print("Could not enumerate devices due to an excepion: " + ex.Message);
+            }
+            */
+            
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Instantiate an Enumerator to find audio devices
+                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                //Get all the devices, no matter what condition or status
+                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                //Loop through all devices
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        dev.AudioEndpointVolume.VolumeStepUp();
+                        Trace.WriteLine(dev.AudioEndpointVolume.MasterVolumeLevel);
+                        trackBar1.Value=(int) dev.AudioEndpointVolume.MasterVolumeLevel;
+                        //Trace.WriteLine(trackBar1.Value);
+                        //Set at maximum volume
+                        //dev.AudioEndpointVolume.MasterVolumeLevel = 0;
+
+                        //Get its audio volume
+                        System.Diagnostics.Debug.Print("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevel.ToString());
+
+                        //Mute it
+                        //dev.AudioEndpointVolume.Mute = true;
+                        System.Diagnostics.Debug.Print(dev.FriendlyName + " is muted");
+                    }
+                    catch (Exception ex)
+                    {
+                        //Do something with exception when an audio endpoint could not be muted
+                        System.Diagnostics.Debug.Print(dev.FriendlyName + " could not be muted");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //When something happend that prevent us to iterate through the devices
+                System.Diagnostics.Debug.Print("Could not enumerate devices due to an excepion: " + ex.Message);
+            }
+            
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Instantiate an Enumerator to find audio devices
+                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                //Get all the devices, no matter what condition or status
+                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                //Loop through all devices
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        dev.AudioEndpointVolume.VolumeStepDown();
+                        Trace.WriteLine(dev.AudioEndpointVolume.MasterVolumeLevel);
+                        trackBar1.Value = (int)dev.AudioEndpointVolume.MasterVolumeLevel;
+                        //Trace.WriteLine(trackBar1.Value);
+                        //Set at maximum volume
+                        //dev.AudioEndpointVolume.MasterVolumeLevel = 0;
+
+                        //Get its audio volume
+                        System.Diagnostics.Debug.Print("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevel.ToString());
+
+                        //Mute it
+                        //dev.AudioEndpointVolume.Mute = true;
+                        System.Diagnostics.Debug.Print(dev.FriendlyName + " is muted");
+                    }
+                    catch (Exception ex)
+                    {
+                        //Do something with exception when an audio endpoint could not be muted
+                        System.Diagnostics.Debug.Print(dev.FriendlyName + " could not be muted");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //When something happend that prevent us to iterate through the devices
+                System.Diagnostics.Debug.Print("Could not enumerate devices due to an excepion: " + ex.Message);
+            }
+            
+        }
+    }
+    /*
+    static class NativeMethods
+    {
+
+        [DllImport("winmm.dll", EntryPoint = "waveOutSetVolume")]
+        public static extern int WaveOutSetVolume(IntPtr hwo, uint dwVolume);
+
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        public static extern bool PlaySound(string pszSound, IntPtr hmod, uint fdwSound);
+    }
+
+    public static class MSWindowsFriendlyNames
+    {
+        public static string WindowsXP { get { return "Windows XP"; } }
+        public static string WindowsVista { get { return "Windows Vista"; } }
+        public static string Windows7 { get { return "Windows 7"; } }
+        public static string Windows8 { get { return "Windows 8"; } }
+    }
+
+    public static class SistemVolumChanger
+    {
+        public static void SetVolume(int value)
+        {
+            if (value < 0)
+                value = 0;
+
+            if (value > 100)
+                value = 100;
+
+            var osFriendlyName = GetOSFriendlyName();
+
+            if (osFriendlyName.Contains(MSWindowsFriendlyNames.WindowsXP))
+            {
+                SetVolumeForWIndowsXP(value);
+            }
+            else if (osFriendlyName.Contains(MSWindowsFriendlyNames.WindowsVista) || osFriendlyName.Contains(MSWindowsFriendlyNames.Windows7) || osFriendlyName.Contains(MSWindowsFriendlyNames.Windows8))
+            {
+                SetVolumeForWIndowsVista78(value);
+            }
+            else
+            {
+                SetVolumeForWIndowsVista78(value);
+            }
+        }
+
+        public static int GetVolume()
+        {
+            int result = 100;
+            try
+            {
+                MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+                MMDevice device = DevEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+                result = (int)(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            }
+            catch (Exception)
+            {
+            }
+
+            return result;
+        }
+
+        private static void SetVolumeForWIndowsVista78(int value)
+        {
+            try
+            {
+                MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+                MMDevice device = DevEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+
+                device.AudioEndpointVolume.MasterVolumeLevelScalar = (float)value / 100.0f;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private static void SetVolumeForWIndowsXP(int value)
+        {
+            try
+            {
+                // Calculate the volume that's being set
+                double newVolume = ushort.MaxValue * value / 10.0;
+
+                uint v = ((uint)newVolume) & 0xffff;
+                uint vAll = v | (v << 16);
+
+                // Set the volume
+                int retVal = NativeMethods.WaveOutSetVolume(IntPtr.Zero, vAll);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private static string GetOSFriendlyName()
+        {
+            string result = string.Empty;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                result = os["Caption"].ToString();
+                break;
+            }
+            return result;
+        }
+    }
+    */
+}
